50da32f70d715fcd192b3115a2d867a619000c3e
ferlynC <ferlyn.capulong@mnltechnology.com>
2024-01-30 18:20:08 +0800

    Update INotificationService.cs

--- src/ApplicationCore/Interfaces/INotificationService.cs
+++ src/ApplicationCore/Interfaces/INotificationService.cs
@@ -13,7 +13,7 @@ namespace FAIS.ApplicationCore.Interfaces
         Task<StringInterpolation> GetInterpolationById(int id);
         IReadOnlyCollection<TemplateModel> GetNotificationTemplates();
         Task<Template> GetTemplateById(int id);
-        Task AddStringInterpolation(AddStringInterpolationDTO permissionDTO);
-        Task UpdateStringInterpolation(StringInterpolationDTO permissionDTO);
+        Task AddStringInterpolation(AddStringInterpolationDTO dto);
+        Task UpdateStringInterpolation(StringInterpolationDTO dto);
     }
 }
