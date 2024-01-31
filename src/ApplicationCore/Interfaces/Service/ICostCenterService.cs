215e4f4f5f25eb274b1cf692ea8cd0cb6189be78
positibo-busybee <markfrancis@mybusybee.net>
2024-01-31 13:49:52 +0800

    add subfolders for repository and service interfaces

---
+++ src/ApplicationCore/Interfaces/Service/ICostCenterService.cs
@@ -0,0 +1,16 @@
+﻿using FAIS.ApplicationCore.DTOs;
+using FAIS.ApplicationCore.Entities.Structure;
+using FAIS.ApplicationCore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FAIS.ApplicationCore.Interfaces.Services
+{
+    public interface ICostCenterService
+    {
+        IReadOnlyCollection<CostCenterModel> Get();
+        CostCenter GetById(int id);
+        Task<CostCenter> Add(CostCenterDTO costCenterDTO);
+        Task<CostCenter> Update(CostCenterDTO costCenterDTO);
+    }
+}
