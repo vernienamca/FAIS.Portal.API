74f1b1ddf41fb735f95227f2c803b1d23090dde9
Ferlyn Capulong <154585199+ferlyncaps@users.noreply.github.com>
2024-02-04 02:32:40 +0800

    Update String Interpolation supporting methods

--- src/API/Models/StringInterpolationModel.cs
+++ src/API/Models/StringInterpolationModel.cs
@@ -11,7 +11,8 @@ namespace FAIS.Portal.API.Models
         public char IsActive { get; set; }
         public DateTime StatusDate { get; set; }
         public string NotificationType { get; set; }
-        public string CreatedBy { get; set; }
+        public string CreatedByDisplay { get; set; }
+        public int CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
         public string UpdatedBy { get; set; }
         public DateTime? UpdatedAt { get; set; }
