a567f2d38b886aad3cbd0cacd439e325b72c3217
positibo-busybee <markfrancis@mybusybee.net>
2024-02-08 20:36:13 +0800

    update code based on code review

--- src/Infrastructure/Data/ChartOfAccountsRepository.cs
+++ src/Infrastructure/Data/ChartOfAccountsRepository.cs
@@ -26,37 +26,37 @@ namespace FAIS.Infrastructure.Data
                                join usr in _dbContext.Users.AsNoTracking() on ca.CreatedBy equals usr.Id
                                join usrU in _dbContext.Users.AsNoTracking() on ca.UpdatedBy equals usrU.Id 
                                     into joinedUsers from usrU in joinedUsers.DefaultIfEmpty()
-                               join detail in _dbContext.ChartOfAccountDetails.AsNoTracking() on ca.Id equals detail.ChartOfAccountsId 
-                                    into joinedAccounts from detail in joinedAccounts.DefaultIfEmpty()
-                               join libType in _dbContext.LibraryTypes.AsNoTracking() on ca.AccountGroupId equals libType.Id 
-                                    into joinedTypes from libType in joinedTypes.DefaultIfEmpty()
-                               join libOptions in _dbContext.LibraryOptions.AsNoTracking() on ca.SubAccountGroupId equals libOptions.LibraryTypeId 
-                                    into joinedOptions from libOptions in joinedOptions.DefaultIfEmpty()
+                               join dtl in _dbContext.ChartOfAccountDetails.AsNoTracking() on ca.Id equals dtl.ChartOfAccountsId 
+                                    into joinedAccounts from dtl in joinedAccounts.DefaultIfEmpty()
+                               join libT in _dbContext.LibraryTypes.AsNoTracking() on ca.AccountGroupId equals libT.Id 
+                                    into joinedTypes from libT in joinedTypes.DefaultIfEmpty()
+                               join libO in _dbContext.LibraryOptions.AsNoTracking() on ca.SubAccountGroupId equals libO.LibraryTypeId 
+                                    into joinedOptions from libO in joinedOptions.DefaultIfEmpty()
                                orderby ca.Id descending
                                select new ChartOfAccountModel()
                                {
                                    Id = ca.Id,
-                                   AcountGroup = libType.Name,
+                                   AcountGroup = libT.Name,
                                    IsActive = ca.IsActive == "Y" ? true : false,
                                    RcaGL = ca.RcaGL,
                                    RcaLedgerTitle = ca.RcaLedgerTitle,
                                    RcaSL = ca.RcaSL,
                                    StatusDate = ca.StatusDate,
-                                   SubAcountGroup = libOptions.Description,
+                                   SubAcountGroup = libO.Description,
                                    CreatedBy = $"{usr.FirstName} {usr.LastName}",
                                    CreatedAt = ca.CreatedAt,
                                    UpdatedBy = $"{usrU.FirstName} {usrU.LastName}",
                                    UpdatedAt = ca.UpdatedAt,
                                    ChartOfAccountDetailModel = new ChartOfAccountDetailModel
                                    {
-                                       Id = detail.Id,
-                                       ChartOfAccountsId = detail.ChartOfAccountsId,
-                                       CreatedAt = detail.CreatedAt,
+                                       Id = dtl.Id,
+                                       ChartOfAccountsId = dtl.ChartOfAccountsId,
+                                       CreatedAt = dtl.CreatedAt,
                                        CreatedBy = $"{usr.FirstName} {usr.LastName}",
-                                       DateRemoved = detail.DateRemoved.GetValueOrDefault(),
-                                       GL = detail.GL,
-                                       LedgerTitle = detail.LedgerTitle,
-                                       SL = detail.SL,
+                                       DateRemoved = dtl.DateRemoved.GetValueOrDefault(),
+                                       GL = dtl.GL,
+                                       LedgerTitle = dtl.LedgerTitle,
+                                       SL = dtl.SL,
                                        UpdatedBy = $"{usrU.FirstName} {usrU.LastName}",
                                        UpdatedAt = ca.UpdatedAt,
                                    }
@@ -71,38 +71,38 @@ namespace FAIS.Infrastructure.Data
                                    join usr in _dbContext.Users.AsNoTracking() on ca.CreatedBy equals usr.Id
                                    join usrU in _dbContext.Users.AsNoTracking() on ca.UpdatedBy equals usrU.Id 
                                         into joinedUsers from usrU in joinedUsers.DefaultIfEmpty()
-                                   join detail in _dbContext.ChartOfAccountDetails.AsNoTracking() on ca.Id equals detail.ChartOfAccountsId 
-                                        into joinedAccounts from detail in joinedAccounts.DefaultIfEmpty()
-                                   join libType in _dbContext.LibraryTypes.AsNoTracking() on ca.AccountGroupId equals libType.Id 
-                                        into joinedTypes from libType in joinedTypes.DefaultIfEmpty()
-                                   join libOptions in _dbContext.LibraryOptions.AsNoTracking() on ca.SubAccountGroupId equals libOptions.LibraryTypeId 
-                                        into joinedOptions from libOptions in joinedOptions.DefaultIfEmpty()
+                                   join dtl in _dbContext.ChartOfAccountDetails.AsNoTracking() on ca.Id equals dtl.ChartOfAccountsId 
+                                        into joinedAccounts from dtl in joinedAccounts.DefaultIfEmpty()
+                                   join libT in _dbContext.LibraryTypes.AsNoTracking() on ca.AccountGroupId equals libT.Id 
+                                        into joinedTypes from libT in joinedTypes.DefaultIfEmpty()
+                                   join libO in _dbContext.LibraryOptions.AsNoTracking() on ca.SubAccountGroupId equals libO.LibraryTypeId 
+                                        into joinedOptions from libO in joinedOptions.DefaultIfEmpty()
                                    orderby ca.Id descending
                                    orderby ca.Id descending
                                    select new ChartOfAccountModel()
                                    {
                                        Id = ca.Id,
-                                       AcountGroup = libType.Name,
+                                       AcountGroup = libT.Name,
                                        IsActive = ca.IsActive == "Y" ? true : false,
                                        RcaGL = ca.RcaGL,
                                        RcaLedgerTitle = ca.RcaLedgerTitle,
                                        RcaSL = ca.RcaSL,
                                        StatusDate = ca.StatusDate,
-                                       SubAcountGroup = libOptions.Description,
+                                       SubAcountGroup = libO.Description,
                                        CreatedBy = $"{usr.FirstName} {usr.LastName}",
                                        CreatedAt = ca.CreatedAt,
                                        UpdatedBy = $"{usrU.FirstName} {usrU.LastName}",
                                        UpdatedAt = ca.UpdatedAt,
                                        ChartOfAccountDetailModel = new ChartOfAccountDetailModel
                                        {
-                                           Id = detail.Id,
-                                           ChartOfAccountsId = detail.ChartOfAccountsId,
-                                           CreatedAt = detail.CreatedAt,
+                                           Id = dtl.Id,
+                                           ChartOfAccountsId = dtl.ChartOfAccountsId,
+                                           CreatedAt = dtl.CreatedAt,
                                            CreatedBy = $"{usr.FirstName} {usr.LastName}",
-                                           DateRemoved = detail.DateRemoved.GetValueOrDefault(),
-                                           GL = detail.GL,
-                                           LedgerTitle = detail.LedgerTitle,
-                                           SL = detail.SL,
+                                           DateRemoved = dtl.DateRemoved.GetValueOrDefault(),
+                                           GL = dtl.GL,
+                                           LedgerTitle = dtl.LedgerTitle,
+                                           SL = dtl.SL,
                                            UpdatedBy = $"{usrU.FirstName} {usrU.LastName}",
                                            UpdatedAt = ca.UpdatedAt,
                                        }
