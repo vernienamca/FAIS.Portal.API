e04de8eed89338522109cf55bf7392bed0c6a82d
rayrosure1188 <150649428+rayrosure1188@users.noreply.github.com>
2024-03-07 20:01:24 +0800

    Update on Proforma Entries Get, GetById, Add and Update (#56)

---
+++ src/ApplicationCore/Entities/Structure/ProformaEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FAIS.ApplicationCore.Entities.Structure
+{
+    public class ProformaEntry : BaseEntity<int>
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public int TranTypeSeq { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string IsActive { get; set; }
+
+        [DataMember]
+        public DateTime StatusDate { get; set; }
+
+        [DataMember]
+        public DateTime CreatedAt { get; set; }
+
+        [DataMember]
+        public DateTime? UpdatedAt { get; set; }
+
+        [DataMember]
+        public int CreatedBy { get; set; }
+
+        [DataMember]
+        public int? UpdatedBy { get; set; }
+
+        [DataMember]
+        public IEnumerable<ProformaEntryDetail> ProformaEntryDetails { get; set; }
+    }
+}
