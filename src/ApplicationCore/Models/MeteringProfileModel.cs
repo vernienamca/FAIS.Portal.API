36c061aa01463aa033ceb461575fcbc97af9ad46
ray-rosure <jerard@mybusybee.net>
2024-03-19 20:35:45 +0800

    Add Get Endpoint for Metering Profile

---
+++ src/ApplicationCore/Models/MeteringProfileModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FAIS.ApplicationCore.Models
+{
+    public class MeteringProfileModel
+    {
+        public int Id { get; set; }
+        public int TransGrid { get; set; }
+        public int DistrictSeq { get; set; }
+        public string Customer { get; set; }
+        public string MeteringPointName { get; set; }
+        public int InstallationTypeSeq { get; set; }
+        public int FacilityLocationSeq { get; set; }
+        public string Remarks { get; set; }
+        public int? AdRegionSeq { get; set; }
+        public int? AdProvSeq { get; set; }
+        public int? AdMunSeq { get; set; }
+        public int? AdBrgySeq { get; set; }
+        public string IsActive { get; set; }
+        public DateTime StatusDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
---
+++ src/Infrastructure/Data/MeteringProfilesRepository.cs
@@ -0,0 +1,37 @@
+﻿using FAIS.ApplicationCore.Entities.Structure;
+using FAIS.ApplicationCore.Interfaces;
+using FAIS.ApplicationCore.Interfaces.Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAIS.Infrastructure.Data
+{
+    public class MeteringProfilesRepository : EFRepository<MeteringProfile, int>, IMeteringProfileRepository
+    {
+        public MeteringProfilesRepository(FAISContext context) : base(context)
+        {
+        }
+        public IReadOnlyCollection<MeteringProfile> Get()
+        {
+            return _dbContext.MeteringProfiles.ToList();
+        }
+
+        public MeteringProfile GetById(int id)
+        {
+            var meteringProfile = _dbContext.MeteringProfiles.FirstOrDefault(t => t.Id == id);
+
+            return meteringProfile;
+        }
+
+        public async Task<MeteringProfile> Add(MeteringProfile meteringProfile)
+        {
+            return await AddAsync(meteringProfile);
+        }
+
+        public async Task<MeteringProfile> Update(MeteringProfile meteringProfile)
+        {
+            return await UpdateAsync(meteringProfile);
+        }
+    }
+}
