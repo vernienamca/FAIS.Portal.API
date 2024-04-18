a626057ab887331a3e2b80876642b05bb6c3d293
ferlynC <34639116+ferlynC@users.noreply.github.com>
2024-04-19 03:59:18 +0800

    updates for CRUD for library types

--- src/ApplicationCore/Interfaces/Repository/ILibraryTypeRepository.cs
+++ src/ApplicationCore/Interfaces/Repository/ILibraryTypeRepository.cs
@@ -8,7 +8,7 @@ namespace FAIS.ApplicationCore.Interfaces
     public interface ILibraryTypeRepository
     {
         IReadOnlyCollection<LibraryTypeModel> Get();
-        LibraryType GetById(int id);
+        Task<LibraryType> GetById(int id);
         Task<LibraryType> Add(LibraryType libraryType);
         Task<LibraryType> Update(LibraryType libraryType);
         IReadOnlyCollection<string> GetLibraryCodesById(int id, string libraryCode);
