42b605b25a1dd092416e404d6e2f20cd2b09c8fe
ferlynC <34639116+ferlynC@users.noreply.github.com>
2024-05-16 10:17:36 +0800

    plant information updated table, created entity configuration and object class mapper

---
+++ src/ApplicationCore/Entities/Structure/PlantInformation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FAIS.ApplicationCore.Entities.Structure
+{
+    public class PlantInformation : BaseEntity<int>
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string PlantCode { get; set; }
+        [DataMember]
+        public string SubstationName { get; set;}
+        [DataMember]        
+        public string SubstationNameOld { get; set; }
+        [DataMember]
+        public int? ClassSeq{ get; set; }
+        [DataMember]
+        public int TransGrid { get; set; }
+        [DataMember]
+        public int DistrictSeq { get; set; }
+        [DataMember]
+        public int? MidSeq { get; set; }
+        [DataMember]
+        public string? GmapCoord { get; set; }
+        [DataMember]
+        public DateTime? CommissionDate { get; set; }
+        [DataMember]
+        public string UDF1 { get; set; }
+        [DataMember]
+        public string UDF2 { get; set; }
+        [DataMember]
+        public string UDF3 { get; set; }
+        [DataMember]
+        public int? RegionSeq { get; set; }
+        [DataMember]
+        public int? ProvSeq { get; set; }
+        [DataMember]
+        public int? MunSeq { get; set; }
+        [DataMember]
+        public int? BrgySeq { get; set; }
+        [DataMember]
+        public char IsActive { get; set; }
+        [DataMember]
+        public DateTime StatusDate { get; set; }
+        [DataMember]
+        public DateTime CreatedAt { get; set;}
+        [DataMember]
+        public int CreatedBy { get; set;}
+        [DataMember]
+        public DateTime? UpdatedAt { get; set; }
+        [DataMember]
+        public int? UpdatedBy { get; set;}
+
+
+    }
+}
