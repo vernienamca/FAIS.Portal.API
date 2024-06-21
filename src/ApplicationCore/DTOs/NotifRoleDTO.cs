5015017479abe0439a192c0265df9793bff9737e
JmNietoBusyBee <juan@mybusybee.net>
2024-06-21 11:29:23 +0800

    "fixed handling of nullable on DTOs" "fixed warning"

--- src/ApplicationCore/DTOs/NotifRoleDTO.cs
+++ src/ApplicationCore/DTOs/NotifRoleDTO.cs
@@ -5,7 +5,7 @@ namespace FAIS.ApplicationCore.DTOs
     public class NotifRoleDTO
     {
         public List <int> RoleIds { get; set; }
-        public string Id { get; set; }
+        public string Id { get; set; } = string.Empty;
         public string AssetName { get; set; }
         public bool EditMode { get; set; }
         public int ModuleId { get; set; }
