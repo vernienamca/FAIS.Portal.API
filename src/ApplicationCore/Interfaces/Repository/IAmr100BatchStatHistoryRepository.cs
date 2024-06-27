fbfb66d9ddef8f0b99bb0c645e7b3509a8c41412
Ferlyn Capulong <154585199+ferlyncaps@users.noreply.github.com>
2024-06-28 00:22:23 +0800

    AMR Batch Status History mapping and repository

---
+++ src/ApplicationCore/Interfaces/Repository/IAmr100BatchStatHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using FAIS.ApplicationCore.Entities.Structure;
+using FAIS.ApplicationCore.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FAIS.ApplicationCore.Interfaces.Repository
+{
+    public interface IAmr100BatchStatHistoryRepository
+    {
+        IReadOnlyCollection<Amr100BatchStatHistoryModel> Get();
+        Task<Amr100BatchStatHistoryModel> GetById(int id);
+        Task<Amr100BatchStatHistory> Add(Amr100BatchStatHistory amr);
+        Task<Amr100BatchStatHistory> Update(Amr100BatchStatHistory amr);
+    }
+}
---
+++ src/ApplicationCore/Models/Amr100BatchStatHistoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FAIS.ApplicationCore.Models
+{
+    public class Amr100BatchStatHistoryModel
+    {
+        public int Id { get; set; }
+        public int Amr100BatchSeq { get; set; }
+        public int StatusCodeLto { get; set; }
+        public string StatusCodeName { get; set; }
+        public DateTime StatusDate { get; set; }
+        public string StatusRemarks { get; set; }
+        public int CreatedBy { get; set; }
+        public string UserName { get; set;}
+     
+    }
+}
