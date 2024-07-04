a0bbb77bc8a621b2eef22df1b3cf9adb7a8b6785
Ferlyn Capulong <154585199+ferlyncaps@users.noreply.github.com>
2024-07-05 02:47:36 +0800

    initial update for Journal Voucher

---
+++ src/ApplicationCore/Entities/Structure/Jv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FAIS.ApplicationCore.Entities.Structure
+{
+    public class Jv : BaseEntity<int>
+    {
+        [DataMember]
+        public string No { get; set; }
+        [DataMember]
+        public int StatusCodeLto { get; set; }
+        [DataMember]
+        public DateTime StatusDate { get; set; }
+        [DataMember]
+        public int? PreparedBy { get; set; }
+        [DataMember]
+        public int? ReviewedBy { get; set; }
+        [DataMember]
+        public int? ApprovedBy { get; set; }
+        [DataMember]
+        public DateTime CreatedAt { get; set; }
+        [DataMember]
+        public DateTime? UpdatedAt { get; set; }
+        [DataMember]
+        public int CreatedBy { get; set; }
+        [DataMember]
+        public int? UpdatedBy { get; set; }
+    }
+}
