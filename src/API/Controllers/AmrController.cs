8629fcc2a56eb065e36db750c6b4078270ad12e9
Ryan121716 <ryan.t@mybusybee.net>
2024-07-09 16:02:29 +0800

    Uncomment Authorize

--- src/API/Controllers/AmrController.cs
+++ src/API/Controllers/AmrController.cs
@@ -16,7 +16,7 @@ namespace FAIS.Portal.API.Controllers
     [Produces("application/json")]
     [Route("[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class AmrController : ControllerBase
     {
         #region Variables
