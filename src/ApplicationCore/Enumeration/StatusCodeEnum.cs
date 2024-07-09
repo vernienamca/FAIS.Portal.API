2a34a03e72744d2fd25570bf0a00e2dc47566226
Ryan121716 <ryan.t@mybusybee.net>
2024-07-09 13:58:22 +0800

    Amr 100 Batch Status Code Enum

---
+++ src/ApplicationCore/Enumeration/StatusCodeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace FAIS.ApplicationCore.Enumeration
+{
+    public enum StatusCodeEnum
+    {
+        [Description("Open")]
+        Open = 2,
+        [Description("For Review")]
+        ForReview = 3,
+        [Description("Reviewed")]
+        Reviewed = 4,
+        [Description("For Endorsement")]
+        ForEndorsement = 5,
+        [Description("Endorsed")]
+        Endorsed = 6
+    }
+}
