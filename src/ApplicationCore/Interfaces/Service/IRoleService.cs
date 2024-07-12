4c19515264e19b669e41435f7499b4ff9c3dbc64
Vernie Namca <vernie.namca@theretailscore.com>
2024-07-12 09:09:02 +0800

    Implemented updating of role and minimal fixes on other stuff

--- src/ApplicationCore/Interfaces/Service/IRoleService.cs
+++ src/ApplicationCore/Interfaces/Service/IRoleService.cs
@@ -2,7 +2,6 @@
 using FAIS.ApplicationCore.Entities.Security;
 using FAIS.ApplicationCore.Models;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FAIS.ApplicationCore.Interfaces
@@ -11,7 +10,7 @@ namespace FAIS.ApplicationCore.Interfaces
     {
         IReadOnlyCollection<RoleModel> Get();
         Role GetById(int id);
-        Task<Role> Update(int id);
+        Task<Role> Update(RoleDTO roleDTO);
         IReadOnlyCollection<Role> GetUserRolesById(int userId);
         Task<Role> Add(RoleDTO roleDTO);
     }
